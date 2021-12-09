1a17d2efa92b927d96ebfb50941923a7765a7835
m0nch <margarita.davtyan@gmail.com>
2021-12-10 00:51:21 +0400

    Add project files.

---
+++ MyWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace MyWindowsFormsApp
+{
+    partial class StudentsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.grdStudent = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stLastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stFirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.cmbLastName = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grdStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Students Information";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(102, 75);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(358, 22);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(12, 51);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(19, 15);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(12, 79);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(76, 15);
+            this.lblLastName.TabIndex = 0;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(12, 107);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(77, 15);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(12, 135);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(31, 15);
+            this.lblAge.TabIndex = 0;
+            this.lblAge.Text = "Age";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(102, 103);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(358, 22);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(102, 131);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(358, 22);
+            this.txtAge.TabIndex = 4;
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(102, 47);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(358, 22);
+            this.txtId.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(509, 46);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(85, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(509, 74);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(85, 23);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Clik);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(509, 102);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(85, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // grdStudent
+            // 
+            this.grdStudent.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grdStudent.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grdStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdStudent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.stLastName,
+            this.stFirstName,
+            this.stAge});
+            this.grdStudent.Location = new System.Drawing.Point(12, 222);
+            this.grdStudent.MultiSelect = false;
+            this.grdStudent.Name = "grdStudent";
+            this.grdStudent.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdStudent.Size = new System.Drawing.Size(582, 190);
+            this.grdStudent.TabIndex = 6;
+            this.grdStudent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdStudent_CellClick);
+            this.grdStudent.SelectionChanged += new System.EventHandler(this.grdStudent_SelectionChanged);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "Id";
+            this.id.HeaderText = "Student Id";
+            this.id.Name = "id";
+            // 
+            // stLastName
+            // 
+            this.stLastName.DataPropertyName = "LastName";
+            this.stLastName.HeaderText = "LastName";
+            this.stLastName.Name = "stLastName";
+            // 
+            // stFirstName
+            // 
+            this.stFirstName.DataPropertyName = "FirstName";
+            this.stFirstName.HeaderText = "FirstName";
+            this.stFirstName.Name = "stFirstName";
+            // 
+            // stAge
+            // 
+            this.stAge.DataPropertyName = "Age";
+            this.stAge.HeaderText = "Age";
+            this.stAge.Name = "stAge";
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(12, 175);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(139, 15);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Search by LastName";
+            // 
+            // cmbLastName
+            // 
+            this.cmbLastName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cmbLastName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbLastName.FormattingEnabled = true;
+            this.cmbLastName.Location = new System.Drawing.Point(157, 175);
+            this.cmbLastName.Name = "cmbLastName";
+            this.cmbLastName.Size = new System.Drawing.Size(303, 21);
+            this.cmbLastName.TabIndex = 5;
+            this.cmbLastName.SelectedValueChanged += new System.EventHandler(this.cmbLastName_SelectedValueChanged);
+            // 
+            // StudentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 425);
+            this.Controls.Add(this.cmbLastName);
+            this.Controls.Add(this.grdStudent);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentsForm";
+            this.Text = "Students";
+            this.Load += new System.EventHandler(this.StudentsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView grdStudent;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stLastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stFirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stAge;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.ComboBox cmbLastName;
+    }
+}
+
---
+++ MyWindowsFormsApp/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MyWindowsFormsApp
+{
+    public partial class StudentsForm : Form
+    {
+        List<Student> studentsList = new List<Student>();
+        List<string> lastNameList = new List<string>();
+
+        public StudentsForm()
+        {
+            InitializeComponent();
+            //this.Load += delegate { RefreshGridView(); };
+        }
+        private void StudentsForm_Load(object sender, EventArgs e)
+        {
+            studentsList.Add(new Student() { LastName = "Doe", FirstName = "Jhon", Age = 25 });
+            studentsList.Add(new Student() { LastName = "Davis", FirstName = "Jane", Age = 27 });
+            studentsList.Add(new Student() { LastName = "Parker", FirstName = "Sara", Age = 21 });
+            studentsList.Add(new Student() { LastName = "Simpson", FirstName = "Jessica", Age = 21 });
+            studentsList.Add(new Student() { LastName = "Washington", FirstName = "Andre", Age = 21 });
+            studentsList.Add(new Student() { LastName = "Wilson", FirstName = "Garry", Age = 21 });
+            studentsList.Add(new Student() { LastName = "Wiliams", FirstName = "Amanda", Age = 21 });
+            studentsList.Add(new Student() { LastName = "Wanderbuilt", FirstName = "Karen", Age = 21 });
+            studentsList.Add(new Student() { LastName = "Walker", FirstName = "Monika", Age = 21 });
+            studentsList.Add(new Student() { LastName = "Wilmington", FirstName = "Andre", Age = 21 });
+            grdStudent.AutoGenerateColumns = false;
+            grdStudent.DataSource = studentsList;
+            ReadSelectedRow();
+
+            foreach (var item in studentsList)
+            {
+                lastNameList.Add(item.LastName);
+            }
+            {
+                cmbLastName.ValueMember = "Id";
+                cmbLastName.DisplayMember = "LastName";
+                cmbLastName.DataSource = lastNameList;
+                cmbLastName.SelectedIndex = -1;
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            student.LastName = txtLastName.Text;
+            student.FirstName = txtFirstName.Text;
+            student.Age = Convert.ToInt32(txtAge.Text);
+            studentsList.Add(student);
+            RefreshGridView();
+            ReadSelectedRow();
+        }
+        private void btnRemove_Clik(object sender, EventArgs e)
+        {
+            Guid id = Guid.Parse(txtId.Text);
+            Student student = studentsList.Find(x => x.Id == id);
+            studentsList.Remove(student);
+            RefreshGridView();
+            ReadSelectedRow();
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Guid id = Guid.Parse(txtId.Text);
+            Student student = studentsList.Find(x => x.Id == id);
+            int age = Convert.ToInt32(txtAge.Text);
+            student.Age = age;
+            student.LastName = txtLastName.Text;
+            student.FirstName = txtFirstName.Text;
+            RefreshGridView();
+            ReadSelectedRow();
+        }
+        private void grdStudent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            cmbLastName.SelectedItem = txtLastName.Text.ToString();
+            ReadSelectedRow();
+        }
+        private void RefreshGridView()
+        {
+            grdStudent.DataSource = null;
+            grdStudent.DataSource = studentsList;
+        }
+        private void ReadSelectedRow()
+        {
+            if (grdStudent.SelectedRows.Count > 0)
+            {
+                DataGridViewRow row = grdStudent.SelectedRows[0];
+                txtId.Text = row.Cells["id"].Value.ToString();
+                txtLastName.Text = row.Cells["stLastName"].Value.ToString();
+                txtFirstName.Text = row.Cells["stFirstName"].Value.ToString();
+                txtAge.Text = row.Cells["stAge"].Value.ToString();
+            }
+        }
+
+        private void grdStudent_SelectionChanged(object sender, EventArgs e)
+        {
+            if (grdStudent.SelectedRows.Count > 0)
+            {
+                cmbLastName.SelectedItem = txtLastName.Text.ToString();
+                ReadSelectedRow();
+            }
+        }
+
+        private void cmbLastName_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cmbLastName.SelectedIndex != -1)
+            {
+                string lastName = cmbLastName.SelectedValue.ToString();
+                Student student = studentsList.FirstOrDefault(x => x.LastName == lastName);
+                txtId.Text = student.Id.ToString();
+                txtLastName.Text = student.LastName.ToString();
+                txtFirstName.Text = student.FirstName.ToString();
+                txtAge.Text = student.Age.ToString();
+                foreach (DataGridViewRow row in grdStudent.Rows)
+                {
+                    if (row.Cells[1].Value.ToString().Equals(lastName))
+                    {
+                        row.Selected = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public class Student
+    {
+        public Student()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public int Age { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
